1538c2227c831d83263bed239bec4b061264f0cf
Gustavo Porto <gustavo@portosanti.com>
2022-08-30 13:33:09 +0100

    build: use patchelf on postFixup

--- pkg.nix
+++ pkg.nix
@@ -1,27 +1,26 @@
-{ stdenv, dpkg }:
+{ stdenv, lib, dpkg, gcc-unwrapped, glibc }:
 
-let version = "8.0.3.654";
+let 
+  version = "8.0.3.654";
+  rpath = lib.makeLibraryPath [ gcc-unwrapped glibc ] + ":${stdenv.cc.cc.lib}/lib64";
 in stdenv.mkDerivation {
-  pname = "perimeter81";
+  name = "Perimeter81-${version}";
   inherit version;
   src = builtins.fetchurl {
     url =
       "https://static.perimeter81.com/agents/linux/Perimeter81_${version}.deb";
     sha256 = "0m1fyvvkdjvcs09aizvqpzr2xcybffqmpgdymwa4lwz7rizyxy81";
   };
-  nativeBuildInputs = [ dpkg ];
-  unpackPhase = ''
-    dpkg-deb -x $src .
-  '';
+  buildInputs = [ dpkg ];
+  unpackPhase = "true";
   installPhase = ''
-    mkdir -p $out/{bin,opt/share}
-    cp -r opt $out/
-    cp -r usr/share $out/
-
-    cp $out/opt/Perimeter81/perimeter81 $out/bin/
+    mkdir -p $out
+    dpkg -x $src $out
+    cp -av $out/opt/Perimeter81/perimeter81 $out
+    rm -rf $out/opt
   '';
   postFixup = ''
-    substituteInPlace $out/share/applications/perimeter81.desktop \
-      --replace "/opt/Perimeter81/perimeter81" $out/opt/Perimeter81/perimeter81
+    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/perimeter81 || true
+    patchelf --set-rpath ${rpath} $out/perimeter81 || true
     '';
 }
